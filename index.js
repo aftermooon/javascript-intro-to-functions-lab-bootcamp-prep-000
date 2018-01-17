3709969c703cd376fc08152ceb0fc9ed5d94df1d
B I <b.ijadare@gmail.com>
2018-01-17 05:20:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,4 @@ function logShout(string) {console.log(string.toUpperCase())}
 
 function logWhisper(string) {console.log(string.toLowerCase())}
 
-function sayHiToGrandma(string) {if (string.toLowerCase()) {return "I can't hear you!"}}
-function sayHiToGrandma(string) {if (string.toUpperCase()) {return "YES INDEED!"}}
-function sayHiToGrandma(string) {if (string === "I love you, Grandma.") {return "I love you, too."}}
+function sayHiToGrandma(string) {if (string === "I love you, Grandma.") {return "I love you, too."} else if (string.toLowerCase()) {return "I can't hear you!"} else if (string.toUpperCase()) {return "YES INDEED!"}}
