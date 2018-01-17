c151536adecaf462510ac61137a840ed81043c1f
B I <b.ijadare@gmail.com>
2018-01-17 05:32:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function logShout(string) {console.log(string.toUpperCase())}
 function logWhisper(string) {console.log(string.toLowerCase())}
 
 function sayHiToGrandma(string) {if (string === "I love you, Grandma.") {return "I love you, too."}
-else if (string.toLowerCase()) {return "I can't hear you!"}
-else if (string.toUpperCase()) {return "YES INDEED!"}}
+else if (string === string.toLowerCase()) {return "I can't hear you!"}
+else if (string === string.toUpperCase()) {return "YES INDEED!"}}
